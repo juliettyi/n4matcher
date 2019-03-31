a3d894678370267d83976801ed13bf68f0c8b22f
Yi Chen <juliettyichen@gmail.com>
2019-03-31 04:47:13 +0000

    add support for batch

--- feature/feature.py
+++ feature/feature.py
@@ -13,11 +13,30 @@ class FeatureGen(object):
   def load_model(self):
     self._model = VGG16(weights='imagenet', include_top=False)
 
-  def gen_feature(self, img_path):
+  def load_img(self, img_path):
+    '''Load one image.'''
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
+    return x
+  
+  def gen_feature(self, img_path):
+    '''Generate feature for one image.'''
+    x = self.load_img(img_path)
+    # simulate batch size of 1
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    return self.gen_feature_for_batch(x)
+
+  def gen_n_features(self, img_paths):
+    '''Generate feature for many images at the same time.'''
+    x_list = []
+    for img_p in img_paths:
+      x = self.load_img(img_p)
+      x_list.append(x)
+    x = np.stack(x_list, axis=0)
+    return self.gen_feature_for_batch(x)
+
+  def gen_feature_for_batch(self, inp):
+    x = preprocess_input(inp)
     features = self._model.predict(x)
-    return features.reshape([-1])
+    return features.reshape([features.shape[0], -1])
 
---
+++ feature/feature_test.py
@@ -0,0 +1,81 @@
+from feature import FeatureGen
+
+import numpy as np
+import os
+import timeit
+
+fg = FeatureGen()
+
+def batch_size_1():
+  start = timeit.default_timer()
+  feature = fg.gen_feature(os.path.join('test_imgs', '00001.png'))
+  print(feature.shape)
+  now = timeit.default_timer()
+  print('{} secs'.format(now - start))
+
+def batch_size_2():
+  start = timeit.default_timer()
+  x1 = fg.load_img(os.path.join('test_imgs', '00001.png'))
+  x2 = fg.load_img(os.path.join('test_imgs', '00002.png'))
+  x = np.stack([x1, x2], axis=0)
+  features = fg.gen_feature_for_batch(x)
+  print(features.shape)
+  now = timeit.default_timer()
+  print('{} secs'.format(now - start))
+
+def many_files():
+  start = timeit.default_timer()
+  fn_list = [
+      os.path.join('test_imgs', '00001.png'),
+      os.path.join('test_imgs', '00002.png'),
+      os.path.join('test_imgs', '00003.png'),
+      os.path.join('test_imgs', '00004.png')
+      ]
+  features = fg.gen_n_features(fn_list)
+  print(features.shape)
+  now = timeit.default_timer()
+  print('{} secs'.format(now - start))
+
+def read_files():
+  fn_list = [
+      os.path.join('test_imgs', '00001.png'),
+      os.path.join('test_imgs', '00002.png'),
+      os.path.join('test_imgs', '00003.png'),
+      os.path.join('test_imgs', '00004.png')
+      ]
+  for fn in fn_list:
+    start = timeit.default_timer()
+    content = fg.load_img(fn)
+    now = timeit.default_timer()
+    print('{} secs'.format(now - start))
+
+def batch_size():
+  fn_list = [
+      os.path.join('test_imgs', '00001.png'),
+      os.path.join('test_imgs', '00002.png'),
+      os.path.join('test_imgs', '00003.png'),
+      os.path.join('test_imgs', '00004.png')
+      ]
+  c = []
+  for fn in fn_list:
+    start = timeit.default_timer()
+    content = fg.load_img(fn)
+    now = timeit.default_timer()
+    print('{} secs'.format(now - start))
+    c.append(content)
+
+  for _ in range(5):
+    c += c
+    start = timeit.default_timer()
+    f = fg.gen_feature_for_batch(np.stack(c, axis=0))
+    print(f.shape)
+    now = timeit.default_timer()
+    print('batch size: {}, {} secs'.format(len(c), now - start))
+    
+  
+
+read_files()
+batch_size_1()
+batch_size_2()
+many_files()
+batch_size()
