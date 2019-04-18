0f90ab1a89e0f8af625b6f488d815a55fac65820
Yi Chen <juliettyichen@gmail.com>
2019-04-18 07:13:16 +0000

    cleanup

--- demo/simple_reducer.py
+++
@@ -1,9 +0,0 @@
-import json
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer('response', value_deserializer=lambda v: json.loads(v.decode('utf-8')))
-
-for msg in consumer:
-  print(msg)
-  v = msg.value
-  print('got ressponse {}'.format(v))
